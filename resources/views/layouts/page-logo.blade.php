473fb81e616d21e560ca10acdc98e4aa93ce97a1
Ajulll22 <ajulrizki@gmail.com>
2022-07-18 00:32:26 +0700

    update logo

--- resources/views/layouts/page-logo.blade.php
+++ resources/views/layouts/page-logo.blade.php
@@ -1,4 +1,6 @@
 <a href="{{ route('index') }}" class="flex gap-2" style="font-family: 'rubik'">
+    <img src="{{ asset('assets/img/unila.png') }}" class="max-h-8">
+    <img src="{{ asset('assets/img/dpd.png') }}" class="max-h-8">
     <img src="{{ asset('assets/img/logo-icon.png') }}" class="max-h-8">
     <div class="text-2xl font-bold text-cyan-600 ">
         Omnilaw
